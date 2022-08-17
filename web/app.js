24f711d34275638bdd9e85d6258a6306722ea2bc
ariyanconsole <111132784+ariyanconsole@users.noreply.github.com>
2022-08-18 05:09:14 +0600

    Update app.js

--- web/app.js
+++ web/app.js
@@ -2,13 +2,13 @@
 
   
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    apiKey: "AIzaSyDgVAb0SuNzjlzIgqUsiq3kNY2OjuWggfg",
+    authDomain: "console-7016b.firebaseapp.com",
+    databaseURL: "https://console-7016b-default-rtdb.firebaseio.com",
+    projectId: "console-7016b",
+    storageBucket: "console-7016b.appspot.com",
+    messagingSenderId: "1068419991716",
+    appId: "1:1068419991716:android:b34eb31fa10eed7de6fe31"
   }; 
  
 ////Do Not Edit Below This
