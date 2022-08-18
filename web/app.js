0eff02c371e367b270d65f54c641ffda254d4478
ariyanconsole <111132784+ariyanconsole@users.noreply.github.com>
2022-08-18 23:57:18 +0600

    Update app.js

--- web/app.js
+++ web/app.js
@@ -2,13 +2,13 @@
 
   
     var firebaseConfig = {
-    apiKey: "AIzaSyDgVAb0SuNzjlzIgqUsiq3kNY2OjuWggfg",
-    authDomain: "console-7016b.firebaseapp.com",
-    databaseURL: "https://console-7016b-default-rtdb.firebaseio.com",
-    projectId: "console-7016b",
-    storageBucket: "console-7016b.appspot.com",
-    messagingSenderId: "1068419991716",
-    appId: "1:1068419991716:android:b34eb31fa10eed7de6fe31"
+    apiKey: "AIzaSyAI1HnYzC47H-qQKttX2uXWBDUUYTZV43k",
+    authDomain: "ariyan-console.firebaseapp.com	",
+    databaseURL: "https://ariyan-console-default-rtdb.firebaseio.com",
+    projectId: "ariyan-console",
+    storageBucket: "ariyan-console.appspot.com",
+    messagingSenderId: "126940221349",
+    appId: "1:126940221349:android:314ecc6ec830fcfc43a24c"
   }; 
  
 ////Do Not Edit Below This
